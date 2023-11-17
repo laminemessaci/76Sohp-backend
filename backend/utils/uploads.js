773cd2e06eea7d84096de8d61700c5f2c53b873e
lamminemessaci <lamine.messaci18@gmail.com>
2023-11-17 12:01:55 +0100

    comment func

--- backend/utils/uploads.js
+++ backend/utils/uploads.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 const colors = require("colors");
 
+/**
+ * Deletes an uploaded file from the server.
+ *
+ * @param {Object} req - The request object containing the file information.
+ * @param {string} req.file.filename - The name of the uploaded file.
+ * @param {string} req.file.destination - The destination directory of the uploaded file.
+ * @return {void} This function does not return anything.
+ */
 function deleteUploadedFile(req) {
   const { filename, destination } = req.file;
   const basePath = `${destination}/${filename}`;
